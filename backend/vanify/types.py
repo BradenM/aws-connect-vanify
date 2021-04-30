9f98afc60fc124b361e9f1ed6b725242411286f8
Braden Mars <bradenmars@bradenmars.me>
2021-04-30 03:49:06 -0500

    feat(backend): add connect ContactFlow event typings

---
+++ backend/vanify/types.py
@@ -0,0 +1,38 @@
+"""AWS Connect Vanify Types."""
+
+from typing import Dict, Literal, Optional, TypedDict
+
+ContactFlowInitMethod = Literal["INBOUND", "OUTBOUND", "TRANSFER", "CALLBACK", "API"]
+ContactFlowChannel = Literal["VOICE", "CHAT"]
+
+
+class ContactFlowEndpoint(TypedDict):
+    Address: str
+    Type: str
+
+
+class ContactFlowQueue(TypedDict):
+    ARN: str
+    Name: str
+
+
+class ConnectEventContactData(TypedDict):
+    Attributes: Dict[str, str]
+    Channel: ContactFlowChannel
+    ContactId: str
+    InitialContactId: str
+    InitiationMethod: ContactFlowInitMethod
+    InstanceArn: str
+    PreviousContactId: str
+    Queue: Optional[ContactFlowQueue]
+    SystemEndpoint: Optional[ContactFlowEndpoint]
+    MediaStreams: Dict
+
+
+class ConnectEventDetails(TypedDict):
+    Parameters: Dict[str, str]
+
+
+class ConnectContactFlowEvent(TypedDict):
+    Details: ConnectEventDetails
+    Name: str
