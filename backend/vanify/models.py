ba602090be5b4b682b32c29f20367885eab9e7e9
Braden Mars <bradenmars@bradenmars.me>
2021-05-01 06:56:21 -0500

    feat(models): update VanifyModel model schema, add as_dict method

--- backend/vanify/models.py
+++ backend/vanify/models.py
@@ -1,8 +1,9 @@
 """AWS Connect Vanify Models."""
 
-from datetime import datetime
+from datetime import datetime, timezone
 
 from pynamodb.attributes import UnicodeAttribute, UnicodeSetAttribute, UTCDateTimeAttribute
+from pynamodb.constants import DATETIME_FORMAT
 from pynamodb.models import Model
 
 
@@ -11,8 +12,16 @@ class VanifyModel(Model):
         table_name = "ConnectVanify"
         region = "us-east-1"
 
-    caller_id = UnicodeAttribute(hash_key=True, attr_name="callerId")
+    contact_id = UnicodeAttribute(attr_name="contactId", hash_key=True)
+    caller_id = UnicodeAttribute(range_key=True, attr_name="callerId")
     date = UTCDateTimeAttribute()
-    contact_id = UnicodeAttribute(attr_name="contactId")
     input = UnicodeAttribute(default_for_new=datetime.now)
     results = UnicodeSetAttribute()
+
+    def as_dict(self):
+        """Dump instance as dict."""
+        date = self.date.astimezone(timezone.utc).strftime(DATETIME_FORMAT)
+        attrs = self.attribute_values
+        attrs["date"] = date
+        attrs["results"] = list(self.results)
+        return attrs
