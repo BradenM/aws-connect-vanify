7f2bebd4959bb5b25f589c2e219493b345a8871d
Braden Mars <bradenmars@bradenmars.me>
2021-05-01 08:40:07 -0500

    feat(frontend): setup primevue/primeflex

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import './index.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeflex/primeflex.css'
 
-createApp(App).mount('#app')
+createApp(App).use(PrimeVue).mount('#app')
